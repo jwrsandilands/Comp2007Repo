195fe1d3dba5b757ecd335b74c87c4ae4d54b4bb
Joseph S <55706784+jwrsandilands@users.noreply.github.com>
2021-06-09 20:46:34 +0100

    Feat: Added Cone hit sound effect
    It now makes a bad noise sound effect when you hit a cone and you lose coins.

--- 2007 - CW Street Scene/Assets/Scripts/CollectScore.cs
+++ 2007 - CW Street Scene/Assets/Scripts/CollectScore.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class CollectScore : MonoBehaviour
 {
+    //variables
     public int coinScore;
     public int playerScore;
-    public AudioClip coinAudio;
+    public AudioClip coinAudio, coneAudio;
     public GameObject cameraMic;
     
-
+    //when this object collides with other objects
     private void OnTriggerEnter(Collider other)
     {
+        //is it coin or cone?
         if(other.tag == "Coin")
         {
+            //add the score
             playerScore += coinScore;
+            //destroy and play sound
             Destroy(other.gameObject);
             AudioSource.PlayClipAtPoint(coinAudio, cameraMic.transform.position);
         }
         if(other.tag == "Cone")
         {
+            //subtract the score, but no negative numbers!
             if(playerScore >= 30)
             {
                 playerScore -= coinScore * 3;
@@ -29,7 +34,9 @@ public class CollectScore : MonoBehaviour
                 playerScore -= playerScore;
             }
 
+            //Destroy and play sound
             Destroy(other.gameObject);
+            AudioSource.PlayClipAtPoint(coneAudio, cameraMic.transform.position);
         }
     }
 }
